37115c6b5ad98f9b044ad5f6a0590046c1b7625c
matt-mo <matt.m@justuno.com>
2018-07-13 16:15:46 -0500

    better selector for code tag

--- js/site.js
+++ js/site.js
@@ -91,10 +91,10 @@ $(document).ready(function() {
 
 	init();
 	addEventListener('load', function () {
-		var code = document.querySelector('.code');
+		var code = document.querySelector('.code-example');
 		var worker = new Worker('worker.js');
 		worker.onmessage = function (event) {
-			code[0].innerHTML = event.data;
+			code.innerHTML = event.data;
 		};
 		worker.postMessage(code.textContent);
 	});
